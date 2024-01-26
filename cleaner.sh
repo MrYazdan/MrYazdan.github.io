58eae7e1745171b0da0d290b2f80ae089386940a
Reza Yazdani <mrrezayazdani@yahoo.com>
2024-01-26 23:13:44 +0330

    Create cleaner.sh

---
+++ cleaner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Removes old revisions of snaps
+
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
